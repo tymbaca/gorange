a58553e88635b24092cfb08b530780188837f7d8
Tigran Rostomyan <tumb4ka228@gmail.com>
2025-03-22 03:25:10 +0300

    goodness

--- cmd/election/cache/cache.go
+++ cmd/election/cache/cache.go
@@ -3,6 +3,8 @@ package cache
 import (
 	"sync"
 	"time"
+
+	"github.com/charmbracelet/log"
 )
 
 type Cache struct {
@@ -32,6 +34,8 @@ func (c *Cache) Prolong(key string, ttl time.Duration) {
 
 	record.Deadline = time.Now().Add(ttl)
 	c.data[key] = record
+
+	log.Print("prolong:", key, "by", ttl)
 }
 
 func (c *Cache) SetNX(key, val string, ttl time.Duration) (string, bool) {
@@ -41,11 +45,15 @@ func (c *Cache) SetNX(key, val string, ttl time.Duration) (string, bool) {
 	current, ok := c.data[key]
 	if !ok {
 		c.data[key] = record{Val: val, Deadline: time.Now().Add(ttl)}
+		log.Info("setnx", "key", key, "to", val, "by", ttl)
 		return val, true
 	}
 
 	if current.Deadline.Before(time.Now()) {
+		log.Warn("expired", "key", key)
+
 		c.data[key] = record{Val: val, Deadline: time.Now().Add(ttl)}
+		log.Info("setnx", "key", key, "to", val, "by", ttl)
 		return val, true
 	}
 
--- cmd/election/worker/main.go
+++ cmd/election/worker/main.go
@@ -1,26 +1,56 @@
 package main
 
 import (
-	"log"
+	"context"
 	"math/rand"
+	"os"
+	"os/signal"
 	"strconv"
 	"time"
 
+	"github.com/charmbracelet/log"
 	cacherpc "github.com/tymbaca/gorange/cmd/election/cache/rpc"
 )
 
 func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
 	c := cacherpc.Connect("localhost:1234")
 	id := strconv.Itoa(rand.Intn(100))
-	log.Println("my id is:", id)
+	log.Print("starting", "my-id", id)
 
 	for {
+		if chance(10) {
+			sleep := time.Duration(rand.Intn(10000)) * time.Millisecond
+			log.Error("shotdown! crit-%^ic.. l ER0-OR...", "wake-after", sleep)
+			time.Sleep(sleep)
+
+			select {
+			case <-ctx.Done():
+				log.Fatal("exiting..")
+				return
+			case <-time.After(sleep):
+			}
+		}
+
+		if ctx.Err() != nil {
+			log.Fatal("exiting..")
+			return
+		}
+
 		time.Sleep(time.Second)
 		current, ok, err := c.SetNX("leader", id, 5*time.Second)
 		if err != nil {
 			panic(err)
 		}
 
-		log.Println("current:", current, "set:", ok)
+		log.Info("setnx", "current", current, "set", ok)
 	}
+}
+
+func chance(percent float64) bool {
+	dice := rand.Float64()
+
+	return dice < percent/100
 }
